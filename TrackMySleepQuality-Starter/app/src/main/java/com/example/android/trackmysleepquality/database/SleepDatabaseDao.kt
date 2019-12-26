ed8db8c49d95b76557e4a17d399597ce74c4a665
LiuChunKit <chunkit2796@gmail.com>
2019-12-26 10:59:25 +0800

    Commit 26/12

--- TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,7 +16,70 @@
 
 package com.example.android.trackmysleepquality.database
 
-import androidx.room.Dao
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
 
 @Dao
-interface SleepDatabaseDao
+interface SleepDatabaseDao {}
+
+@Insert
+fun insert(night: SleepNight) {
+}
+
+@Update
+fun update(night: SleepNight){
+
+}
+
+@Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+fun get(key: Long): SleepNight? {
+    return getTonight()
+}
+
+@Query("DELETE FROM daily_sleep_quality_table")
+fun clear(){
+}
+
+@Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+fun getTonight(): SleepNight?{
+    return getTonight()
+}
+
+@Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+fun getAllNights(): LiveData<List<SleepNight>> {
+    return getAllNights()
+}
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
+
+
+
